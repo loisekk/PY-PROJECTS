e26f9eef3cd86713371f4599116fa40cdf3a3722
Yash Brahmankar <yashbrahmankar95@gmail.com>
2025-12-29 03:57:52 +0530

    first commit

---
+++ quize_game.py
@@ -0,0 +1,201 @@
+print("HEY THERE! WELCOME TO THIS QUIZE 😜😉 ")
+
+answer = input("DO YOU WANT TO SOLVE THIS QUIZE 😁 -> ")
+if answer.lower() != "yes":
+    quit
+    print(f"{quit} okk no worries 😊:")
+
+print("LET'S PLAY : ")
+score = 0 
+
+print("Which game WIN'S the tittle of game of the year in 2018 !: ")
+option1 = "marvel's spider man"
+option2 = "god of war"
+option3 = "far cry 5"
+option4 = "assassin's creed odyssey"
+
+print(f"<a> ➡️  {option1}")
+print(f"<b> ➡️  {option2}")
+print(f"<c> ➡️  {option3}")
+print(f"<d> ➡️  {option4}")
+answer = input("WHAT'S DO YOU THINK WHICH ONE IS RIGHT 🤔 : ")
+
+if answer.lower() == "god of war":
+   print("CORRECT BROO 😍  :")
+   score+= 1
+else: 
+    print("YOU'RE WRONG DUDE 🤐 ")
+
+print("pkk let's move on to next que 😜:  ")
+  
+print("Which is the best , most realistic action shooting game b/w this !:")
+option1 = "call of duty"
+option2 = "delta force"
+option3 = "battlefield 6"
+option4 = "valorant"
+
+print(f"<a> ➡️  {option1}")
+print(f"<b> ➡️  {option2}") 
+print(f"<c> ➡️  {option3}")
+print(f"<d> ➡️  {option4}")
+answer = input("you may gotbe confuse bytheway did you play anyone b/w broo 😼 : ")
+
+if answer.lower() == "battlefield 6":
+   print(" AGAIN CORRECT BROO 😻")
+   score+= 1
+else:
+    print("YOU'RE WRONG DUDE 🔫")
+
+
+print("To increase the FPS on the game what is the better way to do that !:")
+option1 = "increse the ram"
+option2 = "lower the grphics"
+option3 = "buy new one"
+option4 = "delete the game if we talk about valorant 😼"
+ 
+print(f"<a> ➡️  {option1}")
+print(f"<b> ➡️  {option2}")
+print(f"<c> ➡️  {option3}")
+print(f"<d> ➡️  {option4}")
+answer = input("WHAT'S DO YOU THINK WHICH ONE IS RIGHT : ")
+
+if answer.lower() == "delete the game if we talk about valorant 😼":
+   print("RIGHT BROO 😭 WE'RE TOO FRUSTRATED ")
+   score+= 1
+else:
+    print("YOU'RE NOT KNOWING WHAT I'M TALKING ABOUT BROO  🙂")
+
+
+print("Which is the best game for RACING 🏎️")
+option1 = "forza horizon"
+option2 = "f1 2k25"
+option3 = "f1 manger"
+option4 = "need for speed"
+
+print(f"<a> ➡️  {option1}")
+print(f"<b> ➡️  {option2}")
+print(f"<c> ➡️  {option3}")
+print(f"<d> ➡️  {option4}")
+answer = input("THIS IS EASY WHEN YOU GET THE REFERENSE 😼:")
+
+if answer.lower() == "f1 2k25":
+   print("CORRECT BROO '😼🏎️' this is the reference")
+   score+= 1
+else:
+    print("YOU'RE WRONG DUDE 🤨")
+
+
+print("Who is the best , called perfect character in 'RESIDENT EVIL' series !:")
+option1 = "leon s-kennedy"
+option2 = "ada wong"
+option3 = "albert wesker"
+option4 = "jill valentine"
+
+print(f"<a> ➡️  {option1}")  
+print(f"<b> ➡️  {option2}")
+print(f"<c> ➡️  {option3}")
+print(f"<d> ➡️  {option4}")
+answer = input("WHAT'S DO YOU THINK WHICH ONE CAN MOGG  : ")
+
+if answer.lower() == "albert wesker":
+   print("CORRECT BROO i think you are a dyy hard fan 🗿 ")
+   score+= 1
+else:
+    print("YOU'RE WRONG DUDE 😡")
+
+
+print("which series has the better way to describe the 'DARKNESS' b/w zombies apocalypse !🌑☠️:") 
+option1 = "resident evil"
+option2 = "left 4 dead"
+option3 = "the walking dead"
+option4 = "dead raising"
+
+print(f"<a> ➡️  {option1}")
+print(f"<b> ➡️  {option2}")
+print(f"<c> ➡️  {option3}")
+print(f"<d> ➡️  {option4}")
+answer = input("THIS MIGHT BE DIFF DEPEND'S ON YOUR DARKNESS LEVEL 🌑: ")
+
+if answer.lower() == "resident evil":
+   print("CORRECT DAMM RIGHT I GOT YOU BROO ☠️🗿 ")
+   score+= 1
+else:
+    print("YOU'RE WRONG DUDE 😵‍💫")
+
+
+print("which is the current best game engine for better graphics and built in tools for making maps and applying physics !:")
+option1 = "cryengine"
+option2 = "unity"
+option3 = "unreal engine 5"
+option4 = "godot engine"
+
+print(f"<a> ➡️  {option1}")
+print(f"<b> ➡️  {option2}")
+print(f"<c> ➡️  {option3}")
+print(f"<d> ➡️  {option4}")
+answer = input("IF YOU ARE PLAYING AAA GAMES YOU ALREADY KNOW THAT 😼: ")
+
+if answer.lower() == "unreal engine 5":
+   print("CORRECT BROO 🗿")
+   score+= 1
+else:
+    print("YOU'RE WRONG DUDE 🥲")
+
+
+print("what is the source to place the mirror in the game's")
+option1 = "create an image"
+option2 = "create an another dimension like 2d"
+option3 = "use slight image which do not work"
+option4 = "use only object to perform the work"
+
+print(f"<a> ➡️  {option1}")
+print(f"<b> ➡️  {option2}") 
+print(f"<c> ➡️  {option3}")
+print(f"<d> ➡️  {option4}")
+answer = input("THIS MIGHT BE TRICKY DID YOU KNOW THAT 😙 : ")
+
+if answer.lower() == "create an another dimension like 2d":
+   print("CORRECT GUESS  BROO 😼")
+   score+= 1
+else:
+    print("DON'T WORRY DUDE THIS ONE IS TRICKY YOU HAVE TO MOVE TO THE NEXT 😜")
+
+
+print("Which is the open world like game and have a best graphics even having the details !:")
+option1 = "gta v"
+option2 = "rdr 2"
+option3 = "far cry 6"
+option4 = "cyberpunk 2077"
+
+print(f"<a> ➡️  {option1}")
+print(f"<b> ➡️  {option2}")
+print(f"<c> ➡️  {option3}")
+print(f"<d> ➡️  {option4}") 
+answer = input("IF YOU'RE PLAYING ALL OF THIS YOU GET CONFIUSED 🤔😙: ")
+
+if answer.lower() == "rdr 2":
+   print("CORRECT BROO 😜 damm correct ")
+   score+= 1
+else:
+    print("YOU'RE WRONG DUDE 🤨")
+
+
+print("which is the strongest charcter ever exist in the game history !;")
+option1 = "kratos"
+option2 = "sun-wukong"
+option3 = "asura"
+option4 = "dante"
+
+print(f"<a> ➡️  {option1}")
+print(f"<b> ➡️  {option2}")
+print(f"<c> ➡️  {option3}")
+print(f"<d> ➡️  {option4}")
+answer = input("DON'T BE CONFUSED MAN YOU'RE ALREADY KNOW WHICH ONE I'M TAKING ABOUT 😼🗿: ")
+
+if answer.lower() == "sun-wukong":
+   print("I GOT YOU BROO YOU ARE DAMM RIGHT YOU ARE THE REAL GAMER BROO🗿😼😙🤺")
+   score+= 1
+else:
+    print("YOU'RE WRONG DUDE 😡")
+
+
