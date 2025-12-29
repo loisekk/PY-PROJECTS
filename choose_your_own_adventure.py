680a1da1cee90dc906a97795351efbd3f60ad76b
YASH BRAHMANKAR <yashbrahmankar95@gmail.com>
2025-12-30 03:37:20 +0530

    Add files via upload

---
+++ choose_your_own_adventure.py
@@ -0,0 +1,28 @@
+name = input("TYPE YOUR NAME BROO 😼 ->  ")
+print("WELCOME TO THIS YOUR OWN ADVENTURE GAME 😜🗺️")
+
+answer = input("YOU ARE ON A LANE , IT HAS COME TO AN END YOU CAN GO LEFT AND RIGHT ! WHICH PATH WOULD YOU LIKE TO GO 🤔 ").lower()
+
+if answer == "left":
+    answer = input("you are teleported into the marvel universe ,you are happy now because you are dyyhard fan of this universe and you can pick the famous 'cap' and 'tony' rivaly :-> ")
+    if answer =="tony":
+        print("NOW FOR CHOOSING THE RIGHT AND THE COOLEST SIDE , YOU CAN GET YOUR OWN ARMOR FOR THE AIRPORT BATTLE 🤖 😁")
+    elif answer == "cap":
+        print("HUH SO YOU PICKED A CAP SIDE NOW BE READY FOR THE CONSEQUENCES 🥲☠️💀 ")
+    else:
+        print("HMM MAYBE YOU WERE THINKING LIKE SITTING BY SIDE WITH 'LOKI' THAT'S A GOOD AND A  SAFE CHOICE .")
+
+elif answer == "right":
+    answer = input("you are teleported into the DC universe ,you are happy now because you are dyyhard fan of this universe and you can fullfill your dream to be a 'BATMAN' /'CATWOMEN' or  be a 'JOKER' ->")
+    if answer == "batman":
+        print("NOW AFTER A LONG TIME YOUR WISH YOU WERE ALWAYS LOOKING FOR IS GOONA BE REAL NOW YOU CAN GO TO SAVE THE GOTHAM CITY 🗿🦇 ")
+    elif answer =="catwomen":
+        print("SO AFTER BECOMING A BATMAN YOU HAVE TO GO AND SAVE THE GOTHAM CITY INSTEAD YOU  BECOME THE POKIE OF CATWOMEN SPEND SOME TIME WITH IT 😼 LIKE YK !")
+
+    else:
+        print("SO LIKE TO BECOME A JOKER HOW PHYSCHO AND IDIOT YOU ARE YOU ARE ALREADY A JOKER 🤡🃏")
+else:
+    print("NOT A VALID OPTION BUDDY !🥲 YOU LOSE .")
+
+
+print("THANKS FOR VISING THE TWO UNIVERSE A FREE TRIP TO YOUR WISHING UNIVERSE ♾️🚀")
